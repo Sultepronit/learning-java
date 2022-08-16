d47ec332f83497a61bde10da67361a43798a5a7d
Sultepronit <sultepronit@gmail.com>
2022-08-16 21:31:01 +0300

    first commit

---
+++ Consp.java
@@ -0,0 +1,6 @@
+//Array of new created objects
+Person[] people = {new Person(1, "Sue"), new Person(8, "Mike"), new Person(33, "Bob")};
+
+//Array converted to ArrayList
+ArrayList<Person> peopleList = new ArrayList<>(Arrays.asList(people));
+
