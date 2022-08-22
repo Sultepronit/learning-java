adcf658bb6749d00bf193d301eceda20013a9c56
Sultepronit <sultepronit@gmail.com>
2022-08-22 13:52:19 +0300

    learned about singleton pattern

--- Advanced-Java/MySQL Maven/src/main/java/com/db/App.java
+++ Advanced-Java/MySQL Maven/src/main/java/com/db/App.java
@@ -1,13 +1,9 @@
 package com.db;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		var db =Database.instance();
+	}
 }
