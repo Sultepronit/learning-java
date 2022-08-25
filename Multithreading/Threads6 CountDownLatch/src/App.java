13d5b040414a068956d0ea42aa8d8c3e5bd04fab
Sultepronit <sultepronit@gmail.com>
2022-08-25 10:51:20 +0300

    learned about countdown latches

---
+++ Multithreading/Threads6 CountDownLatch/src/App.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Processor implements Runnable {
+	private CountDownLatch latch;
+	
+	public Processor(CountDownLatch latch) {
+		this.latch = latch;
+	}
+	
+	public void run() {
+		System.out.println("Started.");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		latch.countDown(); //counts down every time from 3 and so on 
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		CountDownLatch latch = new CountDownLatch(3);
+		
+		ExecutorService executor = Executors.newFixedThreadPool(3);
+		
+		for(int i = 0; i < 3; i++) {
+			executor.submit(new Processor(latch));
+		}
+		
+		try {
+			latch.await(); // waits for latch == 0
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Completed.");
+	}
+
+}
