6852ce29f44218bc8e6554937ab5923a3583f60f
Sultepronit <sultepronit@gmail.com>
2022-09-06 18:53:42 +0300

    added first components

--- Advanced-Java/Swing/lib/src/main/java/controller/Controller.java
+++ Advanced-Java/Swing/lib/src/main/java/controller/Controller.java
@@ -2,14 +2,17 @@ package controller;
 
 import gui.MainFrame;
 import gui.MainPanel;
+import gui.MyFrame;
 
 public class Controller {
 	private MainFrame mainFrame;
 	private MainPanel mainPanel;
+	private MyFrame myFrame;
 	
 	public Controller() {
 		mainPanel = new MainPanel();
 		mainFrame = new MainFrame();
+		myFrame = new MyFrame();
 		
 		mainFrame.setContentPane(mainPanel);
 	}
--- Advanced-Java/Swing/lib/src/main/java/gui/MainPanel.java
+++ Advanced-Java/Swing/lib/src/main/java/gui/MainPanel.java
@@ -1,15 +1,33 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class MainPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	
 	public MainPanel() {
-		setBackground(Color.YELLOW);
+		//setBackground(Color.YELLOW);
+		
+		var formLabel = new JLabel("Add User");
+		var nameLabel = new JLabel("Name:");
+		var passLabel = new JLabel("Password:");
+		
+		var nameField = new JTextField();
+		var passFiels = new JTextField();
+		
+		var addButton = new JButton("Save");
+		
+		setLayout(new BorderLayout());
+		add(formLabel, BorderLayout.NORTH);
+		add(nameLabel, BorderLayout.CENTER);
+		
 	}
 
 }
