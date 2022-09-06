29691efcd2143c55c8953d863b261ae79c839072
Sultepronit <sultepronit@gmail.com>
2022-09-06 18:54:22 +0300

    my experimental additional window!

---
+++ Advanced-Java/Swing/lib/src/main/java/gui/MyFrame.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	public MyFrame() {
+		super("Little window");
+		
+		setSize(400, 200);
+		setDefaultCloseOperation(HIDE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+}
