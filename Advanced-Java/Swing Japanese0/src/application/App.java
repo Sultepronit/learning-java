389e42eeb68fa73de68ef8af4541001fed572ccd
Sultepronit <sultepronit@gmail.com>
2022-09-08 22:36:25 +0300

    little changes

--- Advanced-Java/Swing Japanese0/src/application/App.java
+++ Advanced-Java/Swing Japanese0/src/application/App.java
@@ -3,12 +3,20 @@ package application;
 import javax.swing.SwingUtilities;
 
 import gui.MainFrame;
+import gui.WordsPanel;
+import gui.WordsPanel3;
 
 class Controller {
 	private static MainFrame mainFrame;
+	private static WordsPanel wordsPanel;
+	private static WordsPanel3 wordsPanel3;
 	
 	public Controller() {
 		mainFrame = new MainFrame();
+		wordsPanel = new WordsPanel();
+		wordsPanel3 = new WordsPanel3();
+		
+		mainFrame.setContentPane(wordsPanel);
 	}
 }
 
