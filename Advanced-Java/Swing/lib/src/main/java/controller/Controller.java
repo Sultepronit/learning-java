49e6fe30ef1a5504666c1cc401b912b5640c88d2
Sultepronit <sultepronit@gmail.com>
2022-09-14 19:58:02 +0300

    added function of saving from swing form to MySQL database

--- Advanced-Java/Swing/lib/src/main/java/controller/Controller.java
+++ Advanced-Java/Swing/lib/src/main/java/controller/Controller.java
@@ -1,8 +1,17 @@
 package controller;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+
 import gui.MainFrame;
 import gui.MainPanel;
 import gui.MyFrame;
+import model.Database;
+import model.Profile;
+import model.User;
+import model.UserDao;
+import model.UserDaoImpl;
 
 public class Controller {
 	private static MainFrame mainFrame;
@@ -11,17 +20,38 @@ public class Controller {
 	//private static MyPanel myPanel; 
 	
 	public Controller() {
+		var props = Profile.getProperties("db");
+		var db = Database.instance();
+		try {
+			db.connect(props);
+		} catch (SQLException e) {
+			System.out.println("Cannot connect to database!");
+			return;
+		}
+		System.out.println("Connected to database");
+		UserDao userDao = new UserDaoImpl();
+		
 		mainPanel = new MainPanel();
 		mainPanel.setFormListener((username, password) -> {
 			System.out.println(username + ": " + password);
+			userDao.save(new User(username, password));
 		});
 		
 		mainFrame = new MainFrame();
-		//myFrame = new MyFrame();
-		//myPanel = new MyPanel();
 		
 		mainFrame.setContentPane(mainPanel);
-		//mainFrame.setContentPane(myPanel);
+		
+		mainFrame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent event) {
+				try {
+					db.close();
+				} catch (SQLException e) {
+					System.out.println("Cannot close database connection!");
+				}
+			}
+		});
+		
+		
 	}
 	
 	public static void changePanel() {
